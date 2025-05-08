bd93cf65f51201d5c85035077eacc013686b40e5
akira0610 <cphs123rty@gmail.com>
2025-05-08 09:41:21 +0800

    更改了時間的刷新速度跟查找資料的數據

--- java-fetcher/src/EarthquakeFetcher.java
+++ java-fetcher/src/EarthquakeFetcher.java
@@ -9,7 +9,7 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class EarthquakeFetcher {
-    private static final String urlStr = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
+    private static final String urlStr = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
     private static final String outputPath = "./out/usgs_data.json";
 
     public static void main(String[] args) {
@@ -27,7 +27,7 @@ public class EarthquakeFetcher {
         // 先跑一次
         fetchTask.run();
         // 每5分鐘跑一次
-        scheduler.scheduleAtFixedRate(fetchTask, 5, 5, TimeUnit.SECONDS);
+        scheduler.scheduleAtFixedRate(fetchTask, 30, 5, TimeUnit.MINUTES);
     }
 
     private static void fetchEarthquakeData() throws IOException {
